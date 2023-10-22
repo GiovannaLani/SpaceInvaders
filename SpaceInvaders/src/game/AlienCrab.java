c144db91895dface6da21b0efbe41462eb221f74
Giovanna <giovanna.lani@opendeusto.es>
2023-10-22 14:54:25 +0200

    Imágenes del juego

--- SpaceInvaders/src/game/AlienCrab.java
+++ SpaceInvaders/src/game/AlienCrab.java
@@ -4,13 +4,15 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
-public class AlienCrab extends Alien{
+public class AlienCrab extends Alien {
 
 	public AlienCrab(double x, double y, int height, int width, GamePanel p) {
 		super(x, y, height, width, p);
 		try {
-			alienImg1=ImageIO.read(getClass().getResourceAsStream("/player/cangrejo1.png"));
-			alienImg2=ImageIO.read(getClass().getResourceAsStream("/player/cangrejo2.png"));
+			alienImg1 = ImageIO.read(getClass().getResourceAsStream("/images/crab1.png"));
+			alienImg2 = ImageIO.read(getClass().getResourceAsStream("/images/crab2.png"));
+			killImg = ImageIO.read(getClass().getResourceAsStream("/images/alien_kill.png"));
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
--- SpaceInvaders/src/game/AlienSquid.java
+++ SpaceInvaders/src/game/AlienSquid.java
@@ -4,12 +4,13 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
-public class AlienSquid extends Alien{
+public class AlienSquid extends Alien {
 	public AlienSquid(double x, double y, int height, int width, GamePanel p) {
 		super(x, y, height, width, p);
 		try {
-			alienImg1=ImageIO.read(getClass().getResourceAsStream("/player/calamar1.png"));
-			alienImg2=ImageIO.read(getClass().getResourceAsStream("/player/calamar2.png"));
+			alienImg1 = ImageIO.read(getClass().getResourceAsStream("/images/squid1.png"));
+			alienImg2 = ImageIO.read(getClass().getResourceAsStream("/images/squid2.png"));
+			killImg = ImageIO.read(getClass().getResourceAsStream("/images/alien_kill.png"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
