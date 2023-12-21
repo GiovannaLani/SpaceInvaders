1819a16e7dcf75b7c5b95b961028c81f136f9e44
Giovanna <giovanna.lani@opendeusto.es>
2023-12-21 15:50:56 +0100

    Ventana del juego centrada

--- src/game/SpaceInvaders.java
+++ src/game/SpaceInvaders.java
@@ -1,6 +1,13 @@
 package game;
 
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
 
 import domain.Game;
 import domain.Player;
@@ -13,9 +20,18 @@ public class SpaceInvaders extends JFrame{
 
 	public SpaceInvaders(Player player, Game game,  LevelType customLevel, Menu menu) {
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		Border lineBorder = BorderFactory.createLineBorder(Color.GREEN, 3, false);
 		
 		GamePanel gamePanel= new GamePanel(player, game, customLevel, menu, this);
-		add(gamePanel.layeredPane);
+		gamePanel.setBorder(lineBorder);
+		
+		JPanel panelPrincipal = new JPanel(new GridBagLayout());
+		panelPrincipal.setBackground(Color.BLACK);
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.fill = GridBagConstraints.CENTER;
+		panelPrincipal.add(gamePanel.layeredPane, gbc);
+
+		add(panelPrincipal);
 		pack();
 				
 		setLocationRelativeTo(null);
